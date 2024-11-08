b84a457a9f9254a1eddbfc21a0a09c040064e6f0
AlexBl21 <hblancorolon@gmail.com>
2024-11-08 06:53:57 -0500

    primer commit

---
+++ src/main/java/co/edu/ufps/SegundoPrevio/ASegundoPrevioApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.SegundoPrevio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASegundoPrevioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ASegundoPrevioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ufps/SegundoPrevio/ASegundoPrevioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.SegundoPrevio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASegundoPrevioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
